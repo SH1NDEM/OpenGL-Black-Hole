aec655b3985a222d28b956cc9bc2d68dc1cece09
SHIDEN <willmirva@gmail.com>
2026-01-28 15:16:45 +0700

    Добавьте файлы проекта.

---
+++ OpenGL Black Hole/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Silk.NET.OpenGL;
+using Silk.NET.Windowing;
+using Silk.NET.Maths;
+
+class Program
+{
+    static IWindow window;
+    static GL gl;
+
+    static void Main(string[] args)
+    {
+        var options = WindowOptions.Default;
+        options.Size = new Vector2D<int>(800, 600);
+        options.Title = "Silk.NET OpenGL";
+
+        window = Window.Create(options);
+
+        window.Load += OnLoad;
+        window.Render += OnRender;
+
+        window.Run();
+    }
+
+    static void OnLoad()
+    {
+        gl = GL.GetApi(window);
+        gl.ClearColor(0.1f, 0.1f, 0.15f, 1f);
+    }
+
+    static void OnRender(double delta)
+    {
+        gl.Clear((uint)ClearBufferMask.ColorBufferBit);
+    }
+}
