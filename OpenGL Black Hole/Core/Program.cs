2ec8f9c54ab3d414daca742cb24a7c26af8f824f
SH1NDEM <willmirva@gmail.com>
2026-01-30 20:56:55 +0700

    add comments and summary

--- OpenGL Black Hole/Core/Program.cs
+++ OpenGL Black Hole/Core/Program.cs
@@ -6,32 +6,41 @@ namespace OpenGL_Black_Hole.Core;
 
 public class Program
 {
+    // Два поля, которые хранят состояние приложения 
     private static IWindow window;
     private static Renderer renderer;
 
     static void Main(string[] args)
     {
-        var options = WindowOptions.Default;
-        options.Size = new Vector2D<int>(800, 600);
-        options.Title = "OpenGL Black Hole";
+        var options = WindowOptions.Default;        // Старые настройки окна
+        options.Size = new Vector2D<int>(800, 600); // Устанавливаем размер
+        options.Title = "OpenGL Black Hole";        // Заголовок окна
 
-        window = Window.Create(options);
+        window = Window.Create(options);            // Окно на основе настроек
 
-        renderer = new Renderer();
+        renderer = new Renderer();                  // Объект рендера (OpenGL и отрисовка)
 
-        window.Load += OnLoad;
-        window.Render += OnRender;
+        window.Load += OnLoad;                      // Подписываемся на загрузку окна (событие один раз)
+        window.Render += OnRender;                  // Подписываемся на рендер (событие каждый кадр)
 
-        window.Run();
+        window.Run();                               // Запуск главного цикла
     }
 
+    /// <summary>
+    /// Инициализация OpenGL и ресурсов рендера
+    /// </summary>
     private static void OnLoad()
     {
         renderer.Initialize(window);
     }
 
+    /// <summary>
+    /// Метод вызывается каждый кадр, 
+    /// </summary>
+    /// <param name="delta">время, прошедшее с предыдущего кадра</param>
     private static void OnRender(double delta)
     {
-        renderer.Render(delta);
+
+        renderer.Render(delta);                     // Отрисовка кадров в поле window
     }
 }
--- OpenGL Black Hole/Render/Renderer.cs
+++ OpenGL Black Hole/Render/Renderer.cs
@@ -5,18 +5,26 @@ namespace OpenGL_Black_Hole.Render;
 
 public class Renderer
 {
-    private GL gl;
+    
+    private GL gl;      // GL - объект, который содержит все функции OpenGL для текущего контекста
 
+    /// <summary>
+    /// Метод инициализации Renderer
+    /// </summary>
+    /// <param name="window">интерфейс из Silk.NET.Windowing, который описывает окно приложения</param>
     public void Initialize(IWindow window)
     {
-        gl = GL.GetApi(window);
-
-        // Серый фон
-        gl.ClearColor(0.1f, 0.1f, 0.1f, 1f);
+        gl = GL.GetApi(window);     // Получаем OpenGL API для конкретного окна
+        
+        gl.ClearColor(0.1f, 0.1f, 0.1f, 1f);        // Серый фон
     }
-
+    
+    /// <summary>
+    /// Очистка кадра и цветового буфера
+    /// </summary>
+    /// <param name="delta"></param>
     public void Render(double delta)
     {
-        gl.Clear((uint)ClearBufferMask.ColorBufferBit);
+        gl.Clear((uint)ClearBufferMask.ColorBufferBit);     // Очищаем экран перед рисованием нового кадра
     }
 }
